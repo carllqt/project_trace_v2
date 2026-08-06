3e32326b137ac83dd847807ecf3e6afe63a95eec
seideeX <johnxedricalejo07@gmail.com>
2026-08-06 15:39:09 +0800

    save ko muna baka mag error

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,10 +16,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => '123@example.com',
+            'password' => bcrypt('123'),
         ]);
     }
 }
