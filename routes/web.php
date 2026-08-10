59ca8ad4b78323efb21a819c542e388c4a3ffece
carllqt <reycarlmedico@gmail.com>
2026-08-10 16:57:05 +0800

    Welcome pPage Done

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,13 @@
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProcurementController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
+        'canResetPassword' => Route::has('password.request'),
+        'status' => session('status'),
     ]);
 });
 
